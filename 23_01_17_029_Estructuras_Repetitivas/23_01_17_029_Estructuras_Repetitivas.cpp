3b1578e6a5417532706db64ea5f1b8ca7020b4c2
Angel <zap748@amerike.edu.mx>
2023-01-17 16:13:29 -0600

    029_Jose
    Estructuras Repetitivas

---
+++ 23_01_17_029_Estructuras_Repetitivas/23_01_17_029_Estructuras_Repetitivas.cpp
@@ -0,0 +1,24 @@
+// 23_01_17_029_Estructuras_Repetitivas.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// While
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	int i;
+
+	i = 10;
+
+	while (i>=1)
+	{
+		cout << i << endl;
+		i--;
+	}
+
+	_getch();
+	return 0;
+}
+
